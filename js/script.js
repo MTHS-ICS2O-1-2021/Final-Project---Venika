f34b21b1676b70ba4684c0e11689fb1e23bf6b8d
Venika Sem <99354917+venika-sem@users.noreply.github.com>
2022-06-10 17:23:24 +0000

    changed files

---
+++ js/script.js
@@ -0,0 +1,20 @@
+/* Created by: Venika Sem
+ * Created on: June 2022
+ * This file contains the JS functions for index.html 
+*/
+
+'use strict'
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/Final-Project---Venika/sw.js", {
+    scope: "/Final-Project---Venika/",
+  })
+}
+
+/**
+ * This function calculates the pizzas total price
+ */
+ function buttonClicked() {
