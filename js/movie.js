63190057fa8c1bd57288e6ed4475f43ce611c30e
Venika Sem <99354917+venika-sem@users.noreply.github.com>
2022-06-20 17:25:13 +0000

    changed js

--- js/movie.js
+++ js/movie.js
@@ -17,64 +17,34 @@ if (navigator.serviceWorker) {
 /**
  * This function gives user movie type recommendation
  */
- function submit() {
-   // input question 1
-   const firstQuestion = document.getElementById("firstQuestion").value
-   const secondQuestion = document.getElementById("secondQuestion").value
-   const thirdQuestion = document.getElementById("thirdQuestion").value
-   const fourthQuestion = document.getElementById("fourthQuestion").value
+function myButtonClicked() {
+  // input question 1
+  const firstQuestion = document.getElementById("firstQuestion").value
+  const secondQuestion = document.getElementById("secondQuestion").value
+  const thirdQuestion = document.getElementById("thirdQuestion").value
+  const fourthQuestion = document.getElementById("fourthQuestion").value
+  const fifthQuestion = document.getElementById("fifthQuestion").value
 
-   // input question 2
-   const firstQuestion2 = document.getElementById("firstQuestion2").value
-   const secondQuestion2 = document.getElementById("secondQuestion2").value
-   const thirdQuestion2 = document.getElementById("thirdQuestion2").value
-   const fourthQuestion2 = document.getElementById("fourthQuestion2").value
+  // input question 2
+  const firstQuestion2 = document.getElementById("firstQuestion2").value
+  const secondQuestion2 = document.getElementById("secondQuestion2").value
+  const thirdQuestion2 = document.getElementById("thirdQuestion2").value
+  const fourthQuestion2 = document.getElementById("fourthQuestion2").value
 
-   // input question 3
-   const firstQuestion3 = document.getElementById("firstQuestion3").value
-   const secondQuestion3 = document.getElementById("secondQuestion3").value
-   const thirdQuestion3 = document.getElementById("thirdQuestion3").value
-   const fourthQuestion3 = document.getElementById("fourthQuestion3").value
+  // input question 3
+  const firstQuestion3 = document.getElementById("firstQuestion3").value
+  const secondQuestion3 = document.getElementById("secondQuestion3").value
+  const thirdQuestion3 = document.getElementById("thirdQuestion3").value
+  const fourthQuestion3 = document.getElementById("fourthQuestion3").value
 
-   // input question 4
-   const firstQuestion4 = document.getElementById("firstQuestion4").value
-   const secondQuestion4 = document.getElementById("secondQuestion4").value
-   const thirdQuestion4 = document.getElementById("thirdQuestion4").value
-   const fourthQuestion4 = document.getElementById("fourthQuestion4").value
-
-   // output 
-   if (
-     firstQuestion == "firstQuestion" ||
-     secondQuestion2 == "secondQuestion2" ||
-     thirdQuestion3 == "thirdQuestion3" ||
-     fourthQuestion4 == "fourthQuestion4"
-   ) {
-    document.getElementById("submit").innerHTML = location.href = "./horror.html";
-   }
-   else if (
-     firstQuestion2 == "firstQuestion2" ||
-     secondQuestion == "secondQuestion" ||
-     thirdQuestion3 == "thirdQuestion3" ||
-     fourthQuestion == "fourthQuestion"
-   ) {
-    document.getElementById("submit").innerHTML = location.href = "./romance.html";
-   }
-   else if (
-     firstQuestion == "firstQuestion" ||
-     secondQuestion3 == "secondQuestion3" ||
-     thirdQuestion3 == "thirdQuestion3" ||
-     fourthQuestion3 == "fourthQuestion3"
-   ) {
-    document.getElementById("submit").innerHTML = location.href = "./action.html";
-   }
-   else if (
-     firstQuestion3 == "firstQuestion3" ||
-     secondQuestion == "secondQuestion" ||
-     thirdQuestion == "thirdQuestion" ||
-     fourthQuestion4 == "fourthQuestion4"
-   ) {
-    document.getElementById("submit").innerHTML = location.href = "./comedy.html";
-   } else {
-     document.getElementById("submit").innerHTML = location.href = "./action.html";
-     }
+  // output 
+  if (
+     firstQuestion == "firstQuestion" &&
+     firstQuestion2 == "firstQuestion2" &&
+     firstQuestion3 == "firstQuestion3" 
+    ) {
+    document.getElementById("submit").innerHTML = "<h3>ACTION MOVIES!</h3>" 
+    setTimeout(myButtonClicked, 3000);
+    location.href = "./horror.html"
+  }
 }
