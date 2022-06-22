4a1b632118d17f38490654f0c231e85ca0286b9e
Venika Sem <99354917+venika-sem@users.noreply.github.com>
2022-06-22 17:22:06 +0000

    changed js

--- js/movie.js
+++ js/movie.js
@@ -19,45 +19,47 @@ if (navigator.serviceWorker) {
  */
 function myButtonClicked() {
   // input question 1
-  const firstQuestion = document.getElementById("firstQuestion").value
-  const secondQuestion = document.getElementById("secondQuestion").value
-  const thirdQuestion = document.getElementById("thirdQuestion").value
-  const fourthQuestion = document.getElementById("fourthQuestion").value
-  const fifthQuestion = document.getElementById("fifthQuestion").value
+  const firstQuestion = document.getElementById("firstQuestion")
+  const secondQuestion = document.getElementById("secondQuestion")
+  const thirdQuestion = document.getElementById("thirdQuestion")
+  const fourthQuestion = document.getElementById("fourthQuestion")
+  const fifthQuestion = document.getElementById("fifthQuestion")
 
   // input question 2
-  const firstQuestion2 = document.getElementById("firstQuestion2").value
-  const secondQuestion2 = document.getElementById("secondQuestion2").value
-  const thirdQuestion2 = document.getElementById("thirdQuestion2").value
-  const fourthQuestion2 = document.getElementById("fourthQuestion2").value
+  const firstQuestion2 = document.getElementById("firstQuestion2")
+  const secondQuestion2 = document.getElementById("secondQuestion2")
+  const thirdQuestion2 = document.getElementById("thirdQuestion2")
+  const fourthQuestion2 = document.getElementById("fourthQuestion2")
 
   // input question 3
-  const firstQuestion3 = document.getElementById("firstQuestion3").value
-  const secondQuestion3 = document.getElementById("secondQuestion3").value
-  const thirdQuestion3 = document.getElementById("thirdQuestion3").value
-  const fourthQuestion3 = document.getElementById("fourthQuestion3").value
-
+  const firstQuestion3 = document.getElementById("firstQuestion3")
+  const secondQuestion3 = document.getElementById("secondQuestion3")
+  const thirdQuestion3 = document.getElementById("thirdQuestion3")
+  const fourthQuestion3 = document.getElementById("fourthQuestion3")
+  
   // output
-  if (
-    firstQuestion == "firstQuestion" &&
-    firstQuestion2 == "firstQuestion2" &&
-    firstQuestion3 == "firstQuestion3"
-  ) {
+  
+  if (firstQuestion.checked == true && firstQuestion2.checked == true && firstQuestion3.checked == true) {
     document.getElementById("submit").innerHTML
     location.href = "./action.html"
-  } else if (
-    secondQuestion == "secondQuestion" &&
-    secondQuestion2 == "secondQuestion2" &&
-    secondQuestion3 == "secondQuestion3"
-  ) {
+    
+  }
+  
+  if (secondQuestion.checked == true && secondQuestion2.checked == true && secondQuestion3.checked == true) {
     document.getElementById("submit").innerHTML
     location.href = "./romance.html"
-  } else if (
-    fourthQuestion == "fourthQuestion" &&
-    fourthQuestion2 == "fourthQuestion2" &&
-    thirdQuestion3 == "thirdQuestion3"
-  ) {
+    
+  }
+
+  if (fourthQuestion.checked == true && fourthQuestion2.checked == true && thirdQuestion3.checked == true) {
     document.getElementById("submit").innerHTML
     location.href = "./comedy.html"
+    
+  }
+
+  if (fourthQuestion.checked == true && fourthQuestion2.checked == true && thirdQuestion3.checked == true) {
+    document.getElementById("submit").innerHTML
+    location.href = "./comedy.html"
+    
   }
 }
